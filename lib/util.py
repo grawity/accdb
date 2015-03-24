b6f6158cebd2b9bd23eb1246f8e69bc58ff731b9
Mantas Mikulėnas <grawity@gmail.com>
2015-03-24 10:15:34 +0200

    remove the no longer used trace()

--- lib/util.py
+++ lib/util.py
@@ -11,9 +11,6 @@ logger = logging.getLogger("accdb")
 logging.basicConfig(format="accdb/%(module)s %(levelname)s: %(message)s",
                     level=(logging.DEBUG if debug else logging.INFO))
 
-def trace(msg):
-    return logging.info("%s", msg)
-
 def _debug(msg):
     return logging.debug("%s", msg)
 
