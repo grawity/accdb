a5831fa95cb2692830cf8bddb09862833ac7278a
Mantas Mikulėnas <grawity@gmail.com>
2015-09-18 15:59:13 +0300

    main: move generic string functions to string.py

--- lib/app.py
+++ lib/app.py
@@ -2,9 +2,7 @@
 # accdb - account database using human-editable flat files as storage
 
 from __future__ import print_function
-import base64
 import cmd
-import fnmatch
 import os
 import re
 import shlex
@@ -19,101 +17,17 @@ from . import hotpie as oath
 
 from .changeset import Changeset, TextChangeset
 from .clipboard import Clipboard
+from .string import *
 from .util import *
 
 from logging import debug as _debug
 
 # string functions {{{
 
-def b64_pad(string, max=4):
-    n = len(string)
-    if n % max:
-        return string.ljust(n + max - (n % max), "=")
-    else:
-        return string
-
-def ellipsize(string, max):
-    if len(string) > max:
-        return string[:max-1] + "…"
-    else:
-        return string
-
 def split_tags(string):
     string = string.strip(" ,\n")
     items = re.split(Entry.RE_TAGS, string)
     return set(items)
-
-def split_ranges(string):
-    for i in string.split():
-        for j in i.split(","):
-            if "-" in j:
-                x, y = j.split("-", 1)
-                yield int(x), int(y)+1
-            else:
-                yield int(j), int(j)+1
-
-def str_split_escaped(string, sep, max=0):
-    state = 0
-    out = []
-    cur = ""
-    _debug("str_split: <- %r", string)
-    for char in string:
-        _debug("str_split:  char %r state %r", char, state)
-        if state == 0:
-            if char == "\\":
-                state = 1
-            elif char == sep:
-                out.append(cur)
-                cur = ""
-            else:
-                cur += char
-        elif state == 1:
-            cur += char
-            state = 0
-    if cur:
-        out.append(cur)
-    _debug("str_split: -> %r", out)
-    return out
-
-def str_split_qwords(string):
-    #string = string.replace("'", "\\'")
-    return shlex.split(string)
-
-def expand_range(string):
-    items = []
-    for m, n in split_ranges(string):
-        items.extend(range(m, n))
-    return items
-
-def re_compile_glob(glob, flags=None):
-    if flags is None:
-        flags = re.I
-    return re.compile(fnmatch.translate(glob), flags | re.U)
-
-def encode_psk(b):
-    return base64.b32encode(b).decode("us-ascii").rstrip("=")
-
-def decode_psk(s):
-    raw_tag = "{raw} "
-    hex_tag = "{hex} "
-    b64_tag = "{b64} "
-
-    if s.startswith(raw_tag):
-        s = s[len(raw_tag):]
-        return s.encode("utf-8")
-    elif s.startswith(hex_tag):
-        s = s[len(hex_tag):]
-        return bytes.fromhex(s)
-    elif s.startswith(b64_tag):
-        s = s[len(b64_tag):]
-        s = s.replace(" ", "")
-        s = b64_pad(s, 4)
-        return base64.b64decode(s)
-    else:
-        s = s.upper()
-        s = s.replace(" ", "")
-        s = b64_pad(s, 8)
-        return base64.b32decode(s)
 
 # }}}
 
---
+++ lib/string.py
@@ -0,0 +1,89 @@
+import base64
+import fnmatch
+import re
+import shlex
+
+def b64_pad(string, max=4):
+    n = len(string)
+    if n % max:
+        return string.ljust(n + max - (n % max), "=")
+    else:
+        return string
+
+def ellipsize(string, max):
+    if len(string) > max:
+        return string[:max-1] + "…"
+    else:
+        return string
+
+def split_ranges(string):
+    for i in string.split():
+        for j in i.split(","):
+            if "-" in j:
+                x, y = j.split("-", 1)
+                yield int(x), int(y)+1
+            else:
+                yield int(j), int(j)+1
+
+def str_split_escaped(string, sep, max=0):
+    state = 0
+    out = []
+    cur = ""
+    _debug("str_split: <- %r", string)
+    for char in string:
+        _debug("str_split:  char %r state %r", char, state)
+        if state == 0:
+            if char == "\\":
+                state = 1
+            elif char == sep:
+                out.append(cur)
+                cur = ""
+            else:
+                cur += char
+        elif state == 1:
+            cur += char
+            state = 0
+    if cur:
+        out.append(cur)
+    _debug("str_split: -> %r", out)
+    return out
+
+def str_split_qwords(string):
+    #string = string.replace("'", "\\'")
+    return shlex.split(string)
+
+def expand_range(string):
+    items = []
+    for m, n in split_ranges(string):
+        items.extend(range(m, n))
+    return items
+
+def re_compile_glob(glob, flags=None):
+    if flags is None:
+        flags = re.I
+    return re.compile(fnmatch.translate(glob), flags | re.U)
+
+def encode_psk(b):
+    return base64.b32encode(b).decode("us-ascii").rstrip("=")
+
+def decode_psk(s):
+    raw_tag = "{raw} "
+    hex_tag = "{hex} "
+    b64_tag = "{b64} "
+
+    if s.startswith(raw_tag):
+        s = s[len(raw_tag):]
+        return s.encode("utf-8")
+    elif s.startswith(hex_tag):
+        s = s[len(hex_tag):]
+        return bytes.fromhex(s)
+    elif s.startswith(b64_tag):
+        s = s[len(b64_tag):]
+        s = s.replace(" ", "")
+        s = b64_pad(s, 4)
+        return base64.b64decode(s)
+    else:
+        s = s.upper()
+        s = s.replace(" ", "")
+        s = b64_pad(s, 8)
+        return base64.b32decode(s)
