03e011ac82e1fea257bc70d84ce6821e784c6e9c
Mantas Mikulėnas <grawity@gmail.com>
2015-09-18 16:01:00 +0300

    main: move OATH functions to oath_util.py

--- lib/app.py
+++ lib/app.py
@@ -17,6 +17,7 @@ from . import hotpie as oath
 
 from .changeset import Changeset, TextChangeset
 from .clipboard import Clipboard
+from .oath_util import OATHParameters
 from .string import *
 from .util import *
 
@@ -176,56 +177,6 @@ def unwrap_secret(wrapped):
         return clear
     else:
         return wrapped
-
-# }}}
-
-# 'OATHParameters' {{{
-
-class OATHParameters(object):
-    """
-    A collection of OATH parameters for a single site.
-    """
-    def __init__(self, raw_psk, digits=6, otype="totp", window=30,
-                 login=None, issuer=None):
-        if otype not in {"totp"}:
-            lib.err("OATH %r is not supported yet" % otype)
-        self.raw_psk = raw_psk
-        self.digits = digits
-        self.otype = otype
-        self.window = window
-        self.login = login
-        self.issuer = issuer
-
-    @property
-    def text_psk(self):
-        return encode_psk(self.raw_psk)
-
-    def make_uri(self):
-        # https://code.google.com/p/google-authenticator/wiki/KeyUriFormat
-        # TODO: url-encode label & issuer
-        if self.issuer and (self.issuer != self.login):
-            issuer = self.issuer
-            label = "%s:%s" % (issuer, self.login)
-        else:
-            issuer = None
-            label = self.login
-
-        uri = "otpauth://totp/%s?secret=%s" % (label, self.text_psk)
-        if issuer:
-            uri += "&issuer=%s" % issuer
-        if self.digits != 6:
-            uri += "&digits=%d" % self.digits
-
-        return uri
-
-    def generate(self):
-        _debug("generating OTP from: %r",
-               base64.b32encode(self.raw_psk).decode("us-ascii"))
-
-        if self.otype == "totp":
-            return oath.TOTP(self.raw_psk, digits=self.digits, window=self.window)
-        else:
-            lib.err("OATH %r is not supported yet" % self.otype)
 
 # }}}
 
