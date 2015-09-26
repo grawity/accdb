eec587c38dbc845da08d1fc305b7aaa54a9651f2
Mantas Mikulėnas <grawity@gmail.com>
2015-09-26 17:06:48 +0300

    util: try to make the logger similar to perl/bash ones

--- lib/util.py
+++ lib/util.py
@@ -4,10 +4,38 @@ import sys
 
 # logging functions {{{
 
+class FancyFormatter(logging.Formatter):
+    _colors = {
+        logging.DEBUG: "\033[36m",
+    }
+
+    arg0 = "accdb"
+
+    def format(self, record):
+        color = self._colors.get(record.levelno)
+        reset = "\033[m" if color else ""
+
+        prefix = record.levelname.lower()
+        if record.levelno == logging.DEBUG:
+            caller = record.module
+            if record.funcName != "__init__":
+                caller += ":" + record.funcName
+            prefix += " (" + caller + ")"
+        msg = record.msg % record.args
+
+        output = "%(arg0)s: %(color)s%(prefix)s:%(reset)s %(msg)s%(reset)s" % locals()
+        return output
+
 debug = os.environ.get("DEBUG", "")
 
-logging.basicConfig(format="accdb: %(levelname)s: (%(module)s) %(message)s",
-                    level=(logging.DEBUG if debug else logging.INFO))
+handler = logging.StreamHandler()
+formatter = FancyFormatter()
+handler.setFormatter(formatter)
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.DEBUG if debug else logging.INFO)
+
+#logging.basicConfig(format="accdb: %(levelname)s: (%(module)s) %(message)s",
+#                    level=(logging.DEBUG if debug else logging.INFO))
 
 _debug = logging.debug
 
