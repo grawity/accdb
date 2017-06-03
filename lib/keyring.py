d7b1066bb0cbc12dd972ec38a0d905d98ec05c90
Mantas Mikulėnas <grawity@gmail.com>
2017-06-03 17:58:27 +0300

    merge xdg_secret into keyring

--- lib/keyring.py
+++ lib/keyring.py
@@ -1,14 +1,16 @@
 import base64
 import subprocess
 
-from .xdg_secret import *
-
 class Keyring(object):
     def get_kek(self, uuid):
-        raise NotImplementedError()
+        secret = self._get_kek(str(uuid))
+        return base64.b64decode(secret) if secret else None
 
     def store_kek(self, uuid, kek):
-        raise NotImplementedError()
+        label = "accdb master key for {%s}" % uuid
+        secret = base64.b64encode(kek).decode()
+        if not self._store_kek(self, label, secret, str(uuid)):
+            raise Exception("failed to store master key in keyring")
 
 class Prompter(object):
     def get_password(self, desc, **kwargs):
@@ -27,26 +29,34 @@ class PinentryPrompter(Prompter):
             else:
                 return None
 
-class XdgKeyring(Keyring, PinentryPrompter):
-    def get_kek(self, uuid):
-        attrs = [
-            "xdg:schema", "lt.nullroute.Accdb.Kek",
-            "uuid", str(uuid),
-        ]
-        secret = xdg_secret_lookup_secret(attrs)
-        if secret:
-            return base64.b64decode(secret)
-        else:
-            return None
+class XdgKeyring(Keyring):
+    def store(self, label, secret, attrs):
+        with subprocess.Popen(["secret-tool", "store", "--label", label] + attrs,
+                               stdin=subprocess.PIPE) as proc:
+            proc.communicate(secret.encode("utf-8"))
+            return proc.wait() == 0
 
-    def store_kek(self, uuid, kek):
-        label = "accdb master key for {%s}" % uuid
-        secret = base64.b64encode(kek).decode()
+    def lookup(self, attrs):
+        with subprocess.Popen(["secret-tool", "lookup"] + attrs,
+                               stdout=subprocess.PIPE) as proc:
+            return proc.stdout.read().rstrip(b"\n")
+
+    def _search_stdout(self, attrs):
+        return subprocess.call(["secret-tool", "search"] + attrs) == 0
+
+    def clear(self, attrs):
+        return subprocess.call(["secret-tool", "clear"] + attrs) == 0
+
+    def _store_kek(self, label, secret, attrs):
         attrs = [
             "xdg:schema", "lt.nullroute.Accdb.Kek",
-            "uuid", str(uuid),
+            "uuid", uuid,
         ]
-        if xdg_secret_store(label, secret, attrs):
-            return True
-        else:
-            raise Exception("failed to store master key in keyring")
+        return xdg_secret_store(label, secret, attrs)
+
+    def _get_kek(self, uuid):
+        attrs = [
+            "xdg:schema", "lt.nullroute.Accdb.Kek",
+            "uuid", uuid,
+        ]
+        return xdg_secret_lookup_secret(attrs)
--- lib/xdg_secret.py
+++ lib/xdg_secret.py
@@ -1,18 +1,1 @@
 import subprocess
-
-def xdg_secret_store(label, secret, attrs):
-    with subprocess.Popen(["secret-tool", "store", "--label", label] + attrs,
-                           stdin=subprocess.PIPE) as proc:
-        proc.communicate(secret.encode("utf-8"))
-        return proc.wait() == 0
-
-def xdg_secret_lookup_secret(attrs):
-    with subprocess.Popen(["secret-tool", "lookup"] + attrs,
-                           stdout=subprocess.PIPE) as proc:
-        return proc.stdout.read().rstrip(b"\n")
-
-def xdg_secret_search_stdout(attrs):
-    return subprocess.call(["secret-tool", "search"] + attrs) == 0
-
-def xdg_secret_clear(attrs):
-    return subprocess.call(["secret-tool", "clear"] + attrs) == 0
