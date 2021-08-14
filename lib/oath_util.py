cc0a2b8254d9fb6e8e9f39e1ef0a2206726a3f24
Mantas Mikulėnas <grawity@gmail.com>
2021-08-15 00:05:40 +0300

    remove leftovers of SteamTOTP

--- lib/oath_util.py
+++ lib/oath_util.py
@@ -10,13 +10,10 @@ class OATHParameters():
     """
     A collection of OATH parameters for a single site.
     """
-    def __init__(self, raw_psk, digits=6, otype="totp", window=30,
+    def __init__(self, raw_psk, digits=6, window=30,
                  login=None, issuer=None, image=None):
-        if otype not in {"totp"}:
-            Core.err("OATH %r is not supported yet", otype)
         self.raw_psk = raw_psk
         self.digits = digits
-        self.otype = otype
         self.window = window
         self.login = login
         self.issuer = issuer
@@ -54,9 +51,4 @@ class OATHParameters():
         Core.debug("generating TOTP from PSK: %r",
                    base64.b32encode(self.raw_psk).decode("us-ascii"))
 
-        if self.otype == "totp":
-            return oath.TOTP(self.raw_psk, digits=self.digits, window=self.window)
-        elif self.otype == "steam-totp":
-            return oath.SteamTOTP(self.raw_psk)
-        else:
-            Core.err("OATH %r is not supported yet", self.otype)
+        return oath.TOTP(self.raw_psk, digits=self.digits, window=self.window)
